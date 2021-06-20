9fbbed2ce5d254dff0adc7f67c84c0fca07e4bbe
Tingchenlee <lee.ting@northeastern.edu>
2021-06-20 13:36:19 -0400

    added Has_Gas_Reactions folder

---
+++ Has_Gas_Reactions/CSTR_temp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=1:00:00
+#SBATCH --mem=20GB
+#SBATCH --job-name=schneider
+#SBATCH --output=logs/Pt111/temp/schneider.%a.log
+#SBATCH --error=logs/Pt111/temp/schneider.%a.slurm.log
+#SBATCH --partition=short
+#SBATCH --mail-user=lee.ting@northeastern.edu
+#SBATCH --mail-type=FAIL,END
+
+#an array for the job.
+#SBATCH --array=1-11
+
+
+####################################################
+source activate rmg_env
+python -u CSTR_script_temp.py
